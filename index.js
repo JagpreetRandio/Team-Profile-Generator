53007c84e7d4e5e3c93bdcc94047cce1920b6b68
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2023-01-09 23:51:20 -0800

    fixed names for each card and added href to html

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+// Packages needed for code
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateHTML = require('./src/generateHtmlPage');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+
+// Prompts for questions 
+
 
